eb8f769f390029115b8d28c774209c4b40e9cdf1
Mark Christian Tan <markcmtan@gmail.com>
2024-05-25 13:58:40 +0800

    fix: social component - use react-icons and add on hover

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,24 +1,19 @@
-import Image from 'next/image'
 import Link from 'next/link'
+
+import { AiFillLinkedin, AiFillInstagram } from 'react-icons/ai'
 
 export function Socials() {
   return (
     <div className="flex flex-row gap-2 justify-center md:justify-start">
       <Link href={'https://www.linkedin.com/in/kjdelacruz'}>
-        <Image
-          src={`/assets/linkedin.svg`}
-          alt="Kat Linkedin Account"
-          width={64}
-          height={64}
-        />
+        <div className="border-kat-primary border rounded-full p-3 hover:bg-kat-accent">
+          <AiFillLinkedin size={32} />
+        </div>
       </Link>
       <Link href={'https://www.instagram.com/kat.uix'}>
-        <Image
-          src={`/assets/linkedin.svg`}
-          alt="Kat Linkedin Account"
-          width={64}
-          height={64}
-        />
+        <div className="border-kat-primary border rounded-full p-3 hover:bg-kat-accent">
+          <AiFillInstagram size={32} />
+        </div>
       </Link>
     </div>
   )
