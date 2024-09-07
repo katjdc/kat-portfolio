71b4a5a54b4d7a0d8bc1e7db39b50b432f2b21f2
Mark Christian Tan <markcmtan@gmail.com>
2024-09-07 23:18:38 +0800

    fix: updated root page with elements and new section components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,6 +11,12 @@ export default function Home() {
     <main className="relative overflow-hidden md:overflow-visible">
       <Navbar />
       <Wrapper>
+        <Hero />
+        <Services />
+        <div className="absolute bg-[url('/assets/yarn.svg')] bg-no-repeat bg-contain top-[20rem] left-[-5%] xl:top-36 xl:left-0 w-[16rem] h-[16rem] xl:w-[30rem] xl:h-[30rem]"></div>
+        <div className="absolute bg-[url('/assets/cat.svg')] bg-no-repeat bg-contain top-[15rem] right-[-30%] xl:top-40 xl:right-0 w-[20rem] h-[20rem] xl:w-[30rem] xl:h-[30rem] origin-bottom -rotate-45 lg:origin-center lg:-rotate-0"></div>
+        <div className="absolute bg-[url('/assets/swirl.svg')] bg-no-repeat bg-contain top-[48rem] left-[20%] xl:top-[50rem] xl:left-[37%] w-[16rem] h-[16rem] md:w-[25rem] md:h-[25rem]"></div>
+        <Footer />
       </Wrapper>
     </main>
   )
