07e2276e8c29f19aa9dedabb3112d4e5c47f628d
Mark Christian Tan <markcmtan@gmail.com>
2024-09-07 23:15:17 +0800

    fix: move navbar and wrapper from layout to page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,17 @@
 import React from 'react'
 
+import { Footer } from '@/components/footer'
+import { Navbar } from '@/components/navbar'
+import { Hero } from '@/components/sections/hero'
+import { Services } from '@/components/sections/services'
+import { Wrapper } from '@/components/wrapper'
+
 export default function Home() {
   return (
-    <section>
-      <div className="absolute bg-[url('/assets/yarn.svg')] bg-no-repeat bg-contain top-[9rem] left-[-4rem] xl:top-36 xl:left-10 w-[20rem] h-[20rem] xl:w-[30rem] xl:h-[30rem]"></div>
-      <div className="absolute bg-[url('/assets/cat.svg')] bg-no-repeat bg-contain top-[5rem] right-[-12rem] xl:top-40 xl:right-8 w-[20rem] h-[20rem] xl:w-[30rem] xl:h-[30rem] origin-bottom -rotate-45 lg:origin-center lg:-rotate-0"></div>
-      <div className="flex flex-col items-center justify-center text-center w-[75%] mx-auto">
-        <h1 className="mt-14 text-lg text-kat-primary font-semibold">
-          Hello, Katrine Joy Dela Cruz here! I am a
-        </h1>
-        <hr className="w-16 my-5 border-kat-primary" />
-        <h2 className="text-6xl lg:text-9xl font-bold mt-32 mb-10 lg:my-4 text-kat-primary font-heading">
-          UI/UX DESIGNER
-        </h2>
-        <p className="mt-6 text-lg font-semibold text-kat-primary">
-          With 2+ years of making big ideas user-friendly. I focus on the design
-          of software, web, and mobile design to produce beautifully crafted
-          digital products.
-        </p>
-        <button className="mt-8 px-6 py-2 text-lg font-bold text-white bg-kat-primary rounded-xl hover:bg-kat-accent">
-          Get in Touch
-        </button>
-      </div>
-      <div className="absolute bg-[url('/assets/swirl.svg')] bg-no-repeat bg-contain top-[55rem] left-[4rem] xl:top-[45rem] xl:left-[38%] w-[20rem] h-[20rem] md:w-[25rem] md:h-[25rem]"></div>
-    </section>
+    <main className="relative overflow-hidden md:overflow-visible">
+      <Navbar />
+      <Wrapper>
+      </Wrapper>
+    </main>
   )
 }
