0d9f8333b1693ccbd3375450b23adf729fc0c531
Mark Christian Tan <markcmtan@gmail.com>
2024-09-07 23:18:11 +0800

    feat: footer section

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Footer() {
+  return (
+    <footer className="bg-kat-backdrop py-8">
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-kat-mono-1 text-sm">
+          © 2021 Katrine Joy Dela Cruz. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
