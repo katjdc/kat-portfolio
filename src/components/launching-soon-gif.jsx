4bf28e4e5e428895d0690e832c45e08fee319a34
Mark Christian Tan <markcmtan@gmail.com>
2024-10-03 21:54:48 +0800

    chore: moved launching soon gif from public to src components

--- src/components/launching-soon-gif.jsx
+++ src/components/launching-soon-gif.jsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
 
+import launchingSoonGIF from '@/components/assets/gif/launching-soon.gif'
+
 export function LaunchingSoonGIF() {
   return (
     <Image
-      src={`/assets/launching-soon.gif`}
+      src={launchingSoonGIF}
       alt="Launching Soon"
       width={500}
       height={500}
