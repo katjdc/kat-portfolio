9c5d3f6190cdb430e4790f1096bcd146898ada89
Mark Christian Tan <markcmtan@gmail.com>
2024-10-03 21:51:53 +0800

    chore: moved logo svgs from public to src components folder

--- src/components/kat-logo.jsx
+++ src/components/kat-logo.jsx
@@ -1,13 +1,8 @@
 import Image from 'next/image'
 import React from 'react'
 
+import katLogo from '@/components/assets/logo/kat-text-logo.svg'
+
 export function Logo() {
-  return (
-    <Image
-      src={`/logo/kat-text-logo.svg`}
-      alt="Kat Logo"
-      width={120}
-      height={120}
-    />
-  )
+  return <Image src={katLogo} alt="Kat Logo" width={120} height={120} />
 }
